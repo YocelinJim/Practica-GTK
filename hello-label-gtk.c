968e32fb0fb5ceed7d926e56e1cad52450cc69b5
Yocelin Jiménez Sánchez <al2222003314@azc.uam.mx>
2026-03-08 21:12:32 -0600

    Add files via upload

---
+++ hello-label-gtk.c
@@ -0,0 +1,31 @@
+#include <gtk/gtk.h>
+
+static void
+activate_cb (GtkApplication *app,
+             gpointer        user_data)
+{
+    GtkWindow *window;
+    GtkWidget *label;
+
+    window = GTK_WINDOW (gtk_application_window_new (app));
+    gtk_window_set_title (window, "Hello World Title!");
+    gtk_window_set_default_size (window, 400, 300);
+
+    label = gtk_label_new ("Hello World label!");
+    gtk_container_add (GTK_CONTAINER (window), label);
+
+    gtk_widget_show (label);
+    gtk_window_present (window);
+}
+
+int
+main (int argc,
+      char **argv)
+{
+    GtkApplication *app = NULL;
+
+    app = gtk_application_new ("uam.azc.ia.example", 0);
+    g_signal_connect (app, "activate", G_CALLBACK (activate_cb), NULL);
+
+    return g_application_run (G_APPLICATION (app), argc, argv);
+}
